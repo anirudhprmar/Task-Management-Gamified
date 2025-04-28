6db920de8553d518008db756b66f46ffc6d82dc8
Anirudh Parmar <anirudhparmar2004@gmail.com>
2025-04-28 12:57:06 +0530

    Signup page created

--- frontend/src/pages/auth/Signup.jsx
+++ frontend/src/pages/auth/Signup.jsx
@@ -1,37 +1,87 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 import { useSignup } from '../../hooks/useSignup';
-// import {LoaderCircle} from 'lucide-react'
-// import { Navigate } from 'react-router-dom';
+import {MoveRight, LoaderCircle} from 'lucide-react'
+import { Navigate, Link } from 'react-router-dom';
 
 function Signup() {
   const {mutate: signup , isLoading, error} = useSignup()
-  // const [username,setUsername] = useState('')
-  // const [email,setEmail] = useState('')
-  // const [password,setPassword] = useState('')
+
+  const [formData,setFormData] = useState({
+    username:"",
+    email:"",
+    password:"",
+  })
+
+  const handleChange = (e)=>{
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.username]:e.target.value
+    }))
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    signup({ username, email, password },{
+    signup(formData,{
       onSuccess:()=>{
-        // Navigate('/')
+        Navigate('/myDay')
       }
     });
   };
 
-  if (isLoading) {
-    return(
-      <div>
-        <LoaderCircle size={4} className=' animate-spin'/>
-      </div>
-    )
-  }
+ 
 
   return (
-    <div>
+   <main>
+    {/* <section>
+    Start Organizing Your Life Today
+    </section> */}
+    <section>
+      <h1>Ready to Get Shit done ?</h1>
+
       <form onSubmit={handleSubmit}>
 
+        <label htmlFor="username">Username</label>
+        <input
+          name="username"
+          type="text"
+          placeholder="John Doe"
+          value={formData.name}
+          onChange={handleChange}
+        />
+
+        <label htmlFor="email">Email</label>
+        <input
+          name="email"
+          type="email"
+          placeholder="johndoe@gmail.com"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        
+        <label htmlFor="password">Password</label>
+        <input
+          name="password"
+          type="password"
+          placeholder="*******"
+          value={formData.password}
+          onChange={handleChange}
+        />
+
+        <button type="submit" disabled={isLoading}>
+          {isLoading ?  <LoaderCircle size={4} className=' animate-spin'/> : <div>
+            <span>Sign up <MoveRight/> </span>
+          </div>}
+        </button>
+
+        {error && <p className='text-red-800'>{error.message}</p>}
+
+        <div>
+          <p>Already have an account?</p>
+          <Link to={'/login'}>Login Here</Link>
+        </div>
       </form>
-    </div>
+    </section>
+   </main>
   )
 }
 
