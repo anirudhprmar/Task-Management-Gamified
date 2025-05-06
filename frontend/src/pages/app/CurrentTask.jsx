95632010376178538c558c5c54a9dbcb499efd8d
Anirudh Parmar <anirudhparmar2004@gmail.com>
2025-05-06 18:38:35 +0530

    Implement CurrentTask component with todo completion functionality

--- frontend/src/pages/app/CurrentTask.jsx
+++ frontend/src/pages/app/CurrentTask.jsx
@@ -1,12 +1,44 @@
 import React from 'react'
+import { useAllTodos, useCompleteTodo,  } from '../../hooks/useTodos'
+import { useNavigate } from 'react-router'
+import { Loader2 } from 'lucide-react'
 
 function CurrentTask() {
+
+  const {data:todo,isLoading} = useAllTodos()
+  const{mutate:completedThisWorkingTodo}=useCompleteTodo()
+  const navigate = useNavigate()
+
+  if (isLoading) {
+    return <Loader2 className='size-3'/>
+  }
+
+  const handleCompleteTodo = (e)=>{
+    e.preventDefault()
+    completedThisWorkingTodo(currentTodo._id)
+    navigate('/myDay')
+  }
+
+  const currentTodo = ()=>{
+   return todo.filter(todo => todo.inPrgress === 'true')
+  }
+
   return (
     <div>
+
+      {currentTodo.length > 1 ?  
+        <div>
+          <h2>{currentTodo?.title}</h2>
+          <p>{currentTodo?.note}</p>
+          <p>{currentTodo?.inProgress}</p>
+          <button onClick={handleCompleteTodo}>Completed</button>
+        </div> 
+        : "not working on any task"   
+      }
+
+     
       {/* 
-      1. working todo , here it will be focused with an update btn to mention completed
-      3. with timer which will run as you start, pause, reset
-      2. then go back to today's tasks
+      and and a ball or orb pulsating and when done click on completed btn and move to tasks of the day back
       */}
     </div>
   )
