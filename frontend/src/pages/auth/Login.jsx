d0cbaaa9ea95745fb62a4a8a44e2a3bffa137f15
Anirudh Parmar <anirudhparmar2004@gmail.com>
2025-05-12 14:03:49 +0530

    Refactor Login and Signup components for improved UI and navigation; update form handling and styling

--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -1,6 +1,6 @@
 import { useLogin } from '../../hooks/useAuth';
-import {LoaderCircle} from 'lucide-react'
-import { Navigate} from 'react-router-dom';
+import {LoaderCircle,MoveRight} from 'lucide-react'
+import { Link, useNavigate} from 'react-router-dom';
 import {useForm} from 'react-hook-form'
 import toast from 'react-hot-toast';
 
@@ -10,7 +10,7 @@ function Login() {
 
   const { register, handleSubmit } = useForm();
 
-  const navigate = Navigate();
+  const navigate = useNavigate();
 
   const onSubmit = (data) => {
     login(data,{
@@ -25,48 +25,70 @@ function Login() {
   }
 
   return (
-   <main>
-     {/* <section>
-    Start Organizing Your Life Today
-    </section> */}
+   <main className='min-h-screen bg-gray-50 text-gray-950 grid xl:grid-cols-2 mx-auto  container min-w-full grid-cols-1'>
+     <section className='hidden xl:block bg-[#0e1919] text-white'>
+      <div className='text-left py-20 px-4'>
+        <span className='font-bold font-inter xl:text-8xl'>Little wins add up. Don’t lose track of your journey.</span>
+      </div>
+    
+    </section>
 
-    <section>
-      <h1>Get back to getting shit done</h1>
-      <form onSubmit={handleSubmit(onSubmit)}>
+    <section className='py-10'>
+      <div className='flex flex-col items-center'>
 
-      <label htmlFor="email">Email</label>
-      <input
-        name="email"
-        type="email"
-        placeholder="johndoe@gmail.com"
-        {...register("email")}
-      />
+        <div className='text-center py-20 px-5'>
+          <h1 className='font-inter font-bold text-2xl lg:text-5xl xl:text-6xl'>Get back to getting shit done</h1>
+        </div>
 
-      <label htmlFor="password">Password</label>
-      <input
-        name="password"
-        type="password"
-        placeholder="*******
-        "
-        {...register("password")}
-      />
+        <div>
+          <form onSubmit={handleSubmit(onSubmit)}
+          className='flex flex-col px-10 gap-3 '
+          >
+            <div className='flex flex-col  gap-1 xl:w-xl'>
+              <label htmlFor="email" className='text-2xl'>Email</label>
+              <input
+                name="email"
+                type="email"
+                placeholder="johndoe@gmail.com"
+                {...register("email")}
+                className='text-xl px-4 py-2 rounded-lg border border-gray-300 bg-white/5 
+                focus:outline-none focus:ring-2 focus:ring-[#0A3E3E] focus:border-transparent 
+                transition-all duration-300 hover:border-[#0A3E3E]'
+              />
+            </div>
+
+            <div className='flex flex-col gap-1 xl:w-xl'>
+              <label htmlFor="password" className='text-2xl'>Password</label>
+              <input
+                name="password"
+                type="password"
+                placeholder="*******
+                "
+                {...register("password")}
+                className='text-xl px-4 py-2 rounded-lg border border-gray-300 bg-white/5 
+                focus:outline-none focus:ring-2 focus:ring-[#0A3E3E] focus:border-transparent 
+                transition-all duration-300 hover:border-[#0A3E3E]'
+              />
+            </div>
+
 
       <button type="submit" disabled={isLoading}>
-        {isLoading ?  <LoaderCircle size={4} className=' animate-spin'/> : <div>
-          <span>Login <MoveRight/> </span>
+        {isLoading ?  <LoaderCircle size={4} className=' animate-spin'/> : <div className='w-full bg-[#0A3E3E] rounded-xl py-2 mt-3 xl:w-xl cursor-pointer'>
+          <span className='flex gap-3 text-2xl items-center justify-center text-white '>Login <MoveRight/> </span>
         </div>}
       </button>
 
-      {error && <p className='text-red-800'>{error.message}</p>}
-
-        <div>
-          <p>Don't have an account?</p>
-               
-          <a onClick={()=>{
-            navigate('/signup')
-          }}>Signup Here</a> //navigate
+            <div className='flex gap-2 justify-between xl:w-xl'>
+              <p>Don't have an account?</p>
+                  
+              <Link to={'/signup'} className='text-[#0D7B7B]'>Signup Here</Link> 
+            </div>
+        
+          </form>
         </div>
-      </form>
+
+      </div>
+
     </section>
 
    </main>
--- frontend/src/pages/auth/Signup.jsx
+++ frontend/src/pages/auth/Signup.jsx
@@ -1,6 +1,6 @@
 import { useSignup } from '../../hooks/useAuth';
 import {MoveRight, LoaderCircle} from 'lucide-react'
-import { Navigate, Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { useForm } from 'react-hook-form';
 import toast from 'react-hot-toast';
 
@@ -9,11 +9,13 @@ function Signup() {
 
   const {register, handleSubmit } = useForm()
 
+  const navigate = useNavigate()
+
 
   const onSubmit = (data) => {
     signup(data,{
       onSuccess:()=>{
-        Navigate('/myDay')
+        navigate('/myDay')
       }
     });
   };
@@ -25,53 +27,79 @@ function Signup() {
  
 
   return (
-   <main>
-    {/* <section>
-    Start Organizing Your Life Today
-    </section> */}
-    <section>
-      <h1>Ready to Get Shit done ?</h1>
+   <main className='min-h-screen bg-gray-50 text-gray-950 grid lg:grid-cols-2 mx-auto  container min-w-full grid-cols-1'>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
+    <section className='hidden xl:block bg-[#0e1919] text-white'>
+      <div className='text-left py-20 px-4'>
+        <span className='text-7xl font-bold font-inter xl:text-9xl'>  Start Organizing Your Life Today</span>
+      </div>
+    </section> 
 
-        <label htmlFor="username">Username</label>
-        <input
-          name="username"
-          type="text"
-          placeholder="John Doe"
-          {...register("name")}
-        />
-
-        <label htmlFor="email">Email</label>
-        <input
-          name="email"
-          type="email"
-          placeholder="johndoe@gmail.com"
-          {...register("email")}
-        />
-        
-        <label htmlFor="password">Password</label>
-        <input
-          name="password"
-          type="password"
-          placeholder="*******
-          "
-          {...register("password")}
-        />
-
-        <button type="submit" disabled={isLoading}>
-          {isLoading ?  <LoaderCircle size={4} className=' animate-spin'/> : <div>
-            <span>Sign up <MoveRight/> </span>
-          </div>}
-        </button>
-
-        {error && <p className='text-red-800'>{error.message}</p>}
+    <section className='py-10'>
+      <div className='flex flex-col items-center'>
+        <div className='text-center py-20 px-5'>
+          <h1 className='font-inter font-bold text-2xl md:text-5xl xl:text-6xl'>Ready to Get Shit done ?</h1>
+        </div>
 
         <div>
-          <p>Already have an account?</p>
-          <Link to={'/login'}>Login Here</Link>
+          <form onSubmit={handleSubmit(onSubmit)}
+          className='flex flex-col px-10 gap-3'
+          >
+            <div className='flex flex-col gap-1 xl:w-xl'>
+              <label htmlFor="username">Username</label>
+              <input
+                name="username"
+                type="text"
+                placeholder="John Doe"
+                {...register("name")}
+                className='text-xl px-4 py-2 rounded-lg border border-gray-300 bg-white/5 
+                focus:outline-none focus:ring-2 focus:ring-[#0A3E3E] focus:border-transparent 
+                transition-all duration-300 hover:border-[#0A3E3E]'
+              />
+            </div>
+
+            <div className='flex flex-col gap-1 xl:w-xl'>
+              <label htmlFor="email">Email</label>
+              <input
+                name="email"
+                type="email"
+                placeholder="johndoe@gmail.com"
+                {...register("email")}
+                className='text-xl px-4 py-2 rounded-lg border border-gray-300 bg-white/5 
+                focus:outline-none focus:ring-2 focus:ring-[#0A3E3E] focus:border-transparent 
+                transition-all duration-300 hover:border-[#0A3E3E]'
+              />
+            </div>
+
+            <div className='flex flex-col gap-1 xl:w-xl'> 
+              <label htmlFor="password">Password</label>
+              <input
+                name="password"
+                type="password"
+                placeholder="*******
+                "
+                className='text-xl px-4 py-2 rounded-lg border border-gray-300 bg-white/5 
+                focus:outline-none focus:ring-2 focus:ring-[#0A3E3E] focus:border-transparent 
+                transition-all duration-300 hover:border-[#0A3E3E]'
+                {...register("password")}
+              />
+            </div>
+
+
+            <button type="submit" disabled={isLoading}>
+              {isLoading ?  <LoaderCircle size={4} className=' animate-spin'/> : <div className='w-full bg-[#0A3E3E] rounded-xl py-2 mt-3 xl:w-xl cursor-pointer'>
+                <span className='flex gap-3 text-2xl items-center justify-center text-white '>Sign up <MoveRight/> </span>
+              </div>}
+            </button>
+
+            <div className='flex gap-2 justify-between xl:w-xl'>
+              <p>Already have an account?</p>
+              <Link to={'/login'} className='text-[#0D7B7B]'>Login Here</Link>
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
+
     </section>
    </main>
   )
